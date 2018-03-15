abaeb7f54201a7e6670d34823a22b72ab60b14f0
SoulString <zmey3301@gmail.com>
2018-03-15 13:12:44 +0400

    fixes for http

--- template/src/plugins/http.js
+++ template/src/plugins/http.js
@@ -1,32 +1,6 @@
 import {http as defaultConfig} from '../../config/app.config'
 import _ from 'lodash'
 import base64 from 'base-64'
-export default class {
-  constructor (config) {
-    config = _.defaults(config, defaultConfig)
-    this.fetchConfig = {
-      url: config.url,
-      method: config.hasOwnProperty('method') ? config.method : 'GET',
-      credentials: config.hasOwnProperty('credentials') ? config.credentials : 'same-origin',
-      mode: config.hasOwnProperty('mode') ? config.mode : 'no-cors',
-      headers: new Headers()
-    }
-    if (config.hasOwnProperty('data')) {
-      // If we'll send JSON
-      if (config.dataType === 'json') {
-        this.fetchConfig.headers.append('Content-Type', 'application/json')
-        this.fetchConfig.body = JSON.stringify(config.data)
-      // If we'll send formData
-      } else {
-        let payload = new FormData()
-        for (let chunk in config.data) {
-          if (config.data.hasOwnProperty(chunk)) payload.append(chunk, config.data[chunk])
-        }
-        this.fetchConfig.body = payload
-      }
-    }
-  }
-}
 
 export default {
   instance (config) {
@@ -43,8 +17,15 @@ export default {
       fetchConfig.headers.append('Authorization', `basic ${base64.encode(`${login}:${password}`)}`)
     }
     if (config.hasOwnProperty('data')) {
-      // If we'll send JSON
-      if (config.dataType === 'json') {
+      if (fetchConfig.method === 'GET') {
+        let attrList = []
+        for (let attr in config.data) {
+          if (config.data.hasOwnProperty(attr)) attrList.push(encodeURIComponent(attr) + '=' + encodeURIComponent(config.data[attr]))
+        }
+        config.url += `?${attrList.join('&')}`
+        console.log(config.url)
+        // If we'll send JSON
+      } else if (config.dataType === 'json') {
         fetchConfig.headers.append('Content-Type', 'application/json')
         fetchConfig.body = JSON.stringify(config.data)
         // If we'll send formData
@@ -59,6 +40,6 @@ export default {
     return fetch(config.url, fetchConfig).then(response => response.json())
   },
   install (Vue) {
-    Vue.prototype.$http = this.instance()
+    Vue.prototype.$http = this.instance
   }
 }
